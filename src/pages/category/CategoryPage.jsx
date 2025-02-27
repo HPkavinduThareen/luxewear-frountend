0c1108d718f70ad07fb9df2340816e470ba64e25
Kavindu Thareen <kavindutharin@gmail.com>
2025-02-27 11:22:58 +0530

    create frountend

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,33 +1,36 @@
-import {useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
-import products from "../../data/products.json";
-import ProductCards from "../shop/ProductCards.jsx";
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
-export const CategoryPage = () => {
-    const{categoryName} = useParams();
+import products from "../../data/products.json"
+import ProductCards from '../shop/ProductCards';
+
+const CategoryPage = () => {
+    const {categoryName} = useParams();
     const [filteredProducts, setFilteredProducts] = useState([]);
 
-    useEffect(() => {
-        const filtered= products.filter((product) => product.category === categoryName.toLowerCase());
+    useEffect(() =>{
+        const filtered = products.filter((product) => product.category === categoryName.toLowerCase());
+
         setFilteredProducts(filtered);
-    }, [categoryName]);
+    } , [categoryName])
 
     useEffect(() => {
-        window.scrollTo(0,0);
-    });
+        window.scrollTo(0, 0)
+    })
+
     return (
         <>
             <section className='section__container bg-primary-light'>
                 <h2 className='section__header capitalize'>{categoryName}</h2>
-                <p className='section__subheader'>
-                    Browse a  diverse range of categories,from chic dresses to versatile accessories.Elevate your style today!
-                </p>
+                <p className='section__subheader'>Browse a diverse range of categories, from chic dresses to versatile accessories. Elevate your style today!</p>
             </section>
 
-            {/*product card*/}
+            {/* products card */}
             <div className='section__container'>
                 <ProductCards products={filteredProducts}/>
             </div>
         </>
     )
 }
+
+export default CategoryPage
--- src/pages/shop/TrendingProducts.jsx
+++ src/pages/shop/TrendingProducts.jsx
@@ -1,31 +1,36 @@
-import React, {useState} from 'react'
-import ProductCards from "./ProductCards.jsx";
-import product from '../../data/products.json';
-const TrendingProducts = () => {
-    const [visibleProducts,setVisibleProducts] = useState(8);
-    const loadMoreProducts = () => {
-        setVisibleProducts(pevCount => pevCount + 4);
-    }
+import React, { useState } from 'react'
+import ProductCards from './ProductCards'
 
+import products from "../../data/products.json"
+
+const TrendingProducts = () => {
+    const [visibleProducts, setVisibleProducts] = useState(8);
+
+    const loadMoreProducts = () => {
+        setVisibleProducts(prevCount => prevCount + 4)
+    }
     return (
         <section className='section__container product__container'>
             <h2 className='section__header'>Trending Products</h2>
-            <p className='section__subheader mb-12'>Discover the hottest picks: Elevate your style with our curated of trending women's fashion products</p>
+            <p className='section__subheader mb-12'>
+                Discover the Hottest Picks: Elevate Your Style with Our Curated Collection of Trending Women's Fashion Products.
+            </p>
 
-        {/*  product card  */}
+            {/* products card */}
             <div className='mt-12'>
-                <ProductCards products={product.slice(0,visibleProducts)}/>
+            <ProductCards products={products.slice(0, visibleProducts)}/>
             </div>
 
-            {/*load more products button*/}
+            {/* load more products btn */}
             <div className='product__btn'>
                 {
-                    visibleProducts < product.length && (
-                        <button onClick={loadMoreProducts} className='btn'>Load More</button>
+                    visibleProducts < products.length && (
+                        <button className='btn' onClick={loadMoreProducts}>Load More</button>
                     )
                 }
             </div>
         </section>
     )
 }
+
 export default TrendingProducts
